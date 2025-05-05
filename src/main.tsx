e8038ce3aad13a90304afaa7a758f66ffa6fde87
shuheix <ono.shuhei210@gmail.com>
2025-05-05 17:59:04 +0900

    re:init

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+import App from './App.tsx'
 
-const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
-  cache: new InMemoryCache(),
-});
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ApolloProvider client={client} >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
