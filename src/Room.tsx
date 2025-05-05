579e4e53f8577b44894aaa939ebbe672fb41df1e
shuheix <ono.shuhei210@gmail.com>
2025-05-06 04:23:57 +0900

    feat: apollo-linkの設定

--- src/Room.tsx
+++ src/Room.tsx
@@ -1,11 +1,5 @@
 import { gql } from "@apollo/client";
 
-const GET_CHANNELS_QUERY = gql`
-    query GetChannels {
-        channels()
-    }
-`;
-
 export const Room = () => {
   return <div>This is Room Page</div>;
 };
